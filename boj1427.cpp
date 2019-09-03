44badb27d3dcfde798a65b2f3ea21ca3a32ad219
kangseungyoon <45073750+kangseungyoon@users.noreply.github.com>
2019-09-03 21:52:34 +0900

    Add files via upload

---
+++ boj1427.cpp
@@ -0,0 +1,15 @@
+/*#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	string n;
+	cin >> n;
+	sort(n.begin(), n.end());
+	reverse(n.begin(), n.end());
+	cout << n;
+	return 0;
+}*/
