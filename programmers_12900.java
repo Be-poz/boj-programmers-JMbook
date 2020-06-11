f974b1241ef3992bbe1059c98006c2ecdfefe614
kangseungyoon <45073750+kangseungyoon@users.noreply.github.com>
2020-06-11 18:07:22 +0900

    Add files via upload

---
+++ programmers_12900.java
@@ -0,0 +1,21 @@
+package programmers;
+//2xn ≈∏¿œ∏µ
+public class programmers_12900 {
+	public static void main(String [] args) {
+		
+	}
+}
+
+//class Solution {
+//    public int solution(int n) {
+//        int []pibo=new int[n+1];
+//        if(n==1) return 1;
+//        if(n==2) return 2;
+//        pibo[1]=1;pibo[2]=2;
+//        for(int i=3;i<=n;i++){
+//            pibo[i]=pibo[i-1]+pibo[i-2];
+//            pibo[i]%=1000000007;
+//        }
+//        return pibo[n];
+//    }
+//}
