a823a24b22b944f836888ace22119cb2be842121
kangseungyoon <45073750+kangseungyoon@users.noreply.github.com>
2020-09-11 16:44:05 +0900

    Update boj_14425.java

--- boj_14425.java
+++ boj_14425.java
@@ -1,69 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 public class boj_14425 {
+    static int n, m;
+    static class TrieNode{
+        HashMap<Character, TrieNode> childNodes = new HashMap<>();
+        boolean last;
+    }
     static class Trie{
-        private final TrieNode rootNode;
-
+        TrieNode rootNode;
         Trie(){
-            rootNode=new TrieNode();
+            rootNode = new TrieNode();
         }
 
         void insert(String word) {
             TrieNode currentNode = rootNode;
-            for (char c : word.toCharArray()) {
-                if (currentNode.getChildNode().containsKey(c)) {
-                    currentNode = currentNode.getChildNode().get(c);
-                } else {
-                    currentNode.getChildNode().put(c,new TrieNode());
-                    currentNode=currentNode.getChildNode().get(c);
-                }
+            for (Character c : word.toCharArray()) {
+                currentNode = currentNode.childNodes.computeIfAbsent(c, o -> new TrieNode());
             }
-            currentNode.setLastChar();
+            currentNode.last = true;
         }
 
-        boolean contain(String word) {
-            TrieNode currentNode=rootNode;
-            for (char c : word.toCharArray()) {
-                if (currentNode.getChildNode().get(c) != null) {
-                    currentNode=currentNode.getChildNode().get(c);
-                }else return false;
+        boolean contains(String word) {
+            TrieNode currentNode = rootNode;
+            for (Character c : word.toCharArray()) {
+                currentNode = currentNode.childNodes.get(c);
+                if(currentNode==null) return false;
             }
-            return currentNode.isLastChar();
+            return currentNode.last;
         }
     }
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        Trie trie = new Trie();
+        int answer = 0;
+        String[] input = bf.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
 
-    static class TrieNode{
-        private boolean lastChar;
-        private final Map<Character, TrieNode> childNode = new HashMap<>();
-
-        boolean isLastChar(){
-            return lastChar;
-        }
-
-        void setLastChar(){
-            this.lastChar = true;
-        }
-
-        Map<Character,TrieNode> getChildNode(){
-            return childNode;
-        }
-
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int answer=0;
-        Trie trie=new Trie();
-        int n=sc.nextInt();
-        int m=sc.nextInt();
         for (int i = 0; i < n; i++) {
-            String tmp = sc.next();
+            String tmp = bf.readLine();
             trie.insert(tmp);
         }
-        for (int i = 0; i < m; i++)
-            if(trie.contain(sc.next())) answer++;
+        for (int i = 0; i < m; i++) {
+            String tmp = bf.readLine();
+            if (trie.contains(tmp))
+                answer++;
+        }
         System.out.println(answer);
     }
 }
